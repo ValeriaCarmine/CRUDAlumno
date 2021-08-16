7a064012ed22f0b9900ad2e3703a7079a4a138f1
ValeriaCarmine <danavaleriahr@hotmail.com>
2021-08-16 12:55:06 -0500

    Primer commit

---
+++ src/crudalumno/GeneradorNombres.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crudalumno;
+
+/**
+ *
+ * @author Optiplex 780
+ */
+public class GeneradorNombres {
+    
+}
